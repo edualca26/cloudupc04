8d8be47aeb93329ea956aaf201f6a1ef2d4408d6
Eduardo <edualca@gmail.com>
2025-08-09 14:57:40 -0500

    Delete

--- s3-bucket-terraform/terraform/main.tf
+++ s3-bucket-terraform/terraform/main.tf
@@ -4,7 +4,28 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
-  force_destroy = true
+  force_destroy = true  # Esto borra el bucket aunque tenga objetos
+
+  rule {
+    id     = "mover-a-glacier-y-borrar"
+    status = "Enabled"
+
+    filter {
+      tag {
+        key   = "usuario"
+        value = "juan"
+      }
+    }
+
+    transition {
+      days          = 90
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 365
+    }
+  }
 
   tags = {
     Environment = "Dev"
